551ffec44633e105973c1a7aab685803e66d91a6
Anita Aziz <aanitaaziz@gmail.com>
2020-06-07 06:14:55 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React App deployed in Surge by Anita Aziz
         </p>
         <a
           className="App-link"
