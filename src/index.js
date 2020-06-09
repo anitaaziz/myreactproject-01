e1d795ca18e5b3c2a34228cd0eed6a79db683921
Anita Aziz <aanitaaziz@gmail.com>
2020-06-10 03:37:43 +0500

    updated index.css file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+function Hi() {
+  return
+      <div className = "root">
+        <strong>Hello Anita</strong>
+        <p>JSX is a syntax react supports, its simple HTML but at the end a complier convert this syntax in JS in the backgroung</p>
+        <ul>
+          <li>React</li>
+          <li>Import</li>
+          <li>JS Function</li>
+          <li>Components</li>
+        </ul>
+        {"Hi" + 12}
+        {10 + 8 + " Hello"}
+      </div>
+}
+ReactDOM.render(<Hi/>, document.querySelector('#root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
